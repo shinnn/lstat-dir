29ceb39ddfba0ffae34678a9c4acf95005c45e06
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-02-16 18:54:32 +0900

    remove fallback for the envs that doesn't have native Map & Promise
    [BREAKING] drop support for Node 0.10.x

--- index.js
+++ index.js
@@ -7,36 +7,34 @@
 var inspect = require('util').inspect;
 var resolvePath = require('path').resolve;
 
-var ES6Map = require('es6-map');
 var lstat = require('lstat');
 var readdir = require('graceful-fs').readdir;
-var PinkiePromise = require('pinkie-promise');
 
 function pairsToMap(pairs) {
-  return new ES6Map(pairs);
+  return new Map(pairs);
 }
 
 module.exports = function lstatDir(dir) {
-  if (typeof dir !== 'string') {
-    return PinkiePromise.reject(new TypeError(
-      'Expected a path of the directory (string), but got a non-string value ' +
-      inspect(dir) +
-      '.'
-    ));
-  }
+  return new Promise(function(resolve, reject) {
+    if (typeof dir !== 'string') {
+      throw new TypeError(
+        'Expected a path of the directory (string), but got a non-string value ' +
+        inspect(dir) +
+        '.'
+      );
+    }
 
-  if (dir.length === 0) {
-    return PinkiePromise.reject(new Error('Expected a path of the directory, but got \'\' (empty string).'));
-  }
+    if (dir.length === 0) {
+      throw new Error('Expected a path of the directory, but got \'\' (empty string).');
+    }
 
-  return new PinkiePromise(function(resolve, reject) {
     readdir(dir, function(err, arr) {
       if (err) {
         reject(err);
         return;
       }
 
-      resolve(PinkiePromise.all(arr.map(function(path) {
+      resolve(Promise.all(arr.map(function(path) {
         var absolutePath = resolvePath(dir, path);
 
         return lstat(absolutePath).then(function(stat) {
