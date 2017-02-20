79247ca584e768f3aa5a13cec92a40755142d755
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-02-20 10:46:59 +0900

    use const, template literals and arrow function
    [BREAKING] drop support for Node.js < 4

--- index.js
+++ index.js
@@ -4,28 +4,26 @@
 */
 'use strict';
 
-var inspect = require('util').inspect;
-var resolvePath = require('path').resolve;
+const inspect = require('util').inspect;
+const resolve = require('path').resolve;
 
-var lstat = require('lstat');
-var readdir = require('graceful-fs').readdir;
+const lstat = require('lstat');
+const readdir = require('graceful-fs').readdir;
 
 function pairsToMap(pairs) {
   return new Map(pairs);
 }
 
+const TYPE_ERROR = 'Expected a path of the directory';
+
 module.exports = function lstatDir(dir) {
   return new Promise(function(resolve, reject) {
     if (typeof dir !== 'string') {
-      throw new TypeError(
-        'Expected a path of the directory (string), but got a non-string value ' +
-        inspect(dir) +
-        '.'
-      );
+      throw new TypeError(`${TYPE_ERROR} (string), but got a non-string value ${inspect(dir)}.`);
     }
 
     if (dir.length === 0) {
-      throw new Error('Expected a path of the directory, but got \'\' (empty string).');
+      throw new Error(`${TYPE_ERROR}, but got '' (empty string).`);
     }
 
     readdir(dir, function(err, arr) {
@@ -34,12 +32,9 @@ module.exports = function lstatDir(dir) {
         return;
       }
 
-      resolve(Promise.all(arr.map(function(path) {
-        var absolutePath = resolvePath(dir, path);
-
-        return lstat(absolutePath).then(function(stat) {
-          return [absolutePath, stat];
-        });
+      resolve(Promise.all(arr.map(path => {
+        const absolutePath = resolve(dir, path);
+        return lstat(absolutePath).then(stat => [absolutePath, stat]);
       })));
     });
   }).then(pairsToMap);
