a3fadbf4b7eea898b02b557306a3798202d9ddd3
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-10-20 20:05:37 +0900

    use `util.promisify()` to drop `lstat` dependency

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const {lstat} = require('fs');
+const {promisify} = require('util');
 const {resolve} = require('path');
 
-const lstat = require('lstat');
 const readdirSorted = require('readdir-sorted');
+
+const promisifiedLstat = promisify(lstat);
 
 module.exports = async function lstatDir(...args) {
   const [dir] = args;
@@ -11,6 +14,6 @@ module.exports = async function lstatDir(...args) {
 
   return new Map(await Promise.all(paths.map(async path => {
     const absolutePath = resolve(dir, path);
-    return [absolutePath, await lstat(absolutePath)];
+    return [absolutePath, await promisifiedLstat(absolutePath)];
   })));
 };
