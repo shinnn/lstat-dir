0059fe317d0f289f9f98ef09640e056c9552cf13
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-12-20 20:40:02 +0900

    test if it supports file URL of a UNC path
    ref. https://nodejs.org/api/fs.html#fs_url_object_support

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const {dirname, join} = require('path');
+const {hostname} = require('os');
 const {isMap} = require('util').types;
 const {pathToFileURL} = require('url');
 const {symlink, unlink} = require('fs').promises;
@@ -9,25 +10,30 @@ const lstatDir = require('.');
 const test = require('tape');
 
 const nonEssentialFilesRe = /(\.DS_Store|\.git|\.nyc_output|coverage)$/u;
+const expected = [
+	'.editorconfig',
+	'.gitattributes',
+	'.gitignore',
+	'.travis.yml',
+	'index.js',
+	'LICENSE',
+	'node_modules',
+	'package-lock.json',
+	'package.json',
+	'README.md',
+	'test.js'
+];
 
 test('lstatDir()', async t => {
 	const result = await lstatDir(__dirname);
 
 	t.ok(isMap(result), 'should be fulfilled with a Map instance.');
 
-	t.deepEqual([...result.keys()].filter(path => !nonEssentialFilesRe.test(path)), [
-		'.editorconfig',
-		'.gitattributes',
-		'.gitignore',
-		'.travis.yml',
-		'index.js',
-		'LICENSE',
-		'node_modules',
-		'package-lock.json',
-		'package.json',
-		'README.md',
-		'test.js'
-	].map(path => join(__dirname, path)), 'should list all contents in a directory.');
+	t.deepEqual(
+		[...result.keys()].filter(path => !nonEssentialFilesRe.test(path)),
+		expected.map(path => join(__dirname, path)),
+		'should list all contents in a directory.'
+	);
 
 	const dir = dirname(require.resolve('readdir-sorted'));
 	const tmp = join(__dirname, 'test-tmp-dir');
@@ -54,6 +60,19 @@ test('lstatDir()', async t => {
 	} catch (err) {
 		t.equal(err.code, 'ENOTDIR', 'should fail when the target path is not a directory.');
 	}
+
+	t.end();
+});
+
+(process.platform === 'win32' ? test : test.skip)('lstatDir() on Windows', async t => {
+	const uncDir = `\\\\${hostname()}\\${__dirname.replace(/^([a-z]):/ui, '$1$')}\\`;
+	const url = new URL(`file:${uncDir.replace(/\\/ug, '/')}`);
+
+	t.deepEqual(
+		[...(await lstatDir(url)).keys()].filter(path => !nonEssentialFilesRe.test(path)),
+		expected.map(path => `${uncDir}${path}`),
+		'should support file URL of a UNC path.'
+	);
 
 	t.end();
 });
